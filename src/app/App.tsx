3a68f1cc503f3fa4187ddfb7bdb48a213fc2f05e
Rick <73193802+Rickovald@users.noreply.github.com>
2023-12-07 13:00:03 +0300

    Initial commit

---
+++ src/app/App.tsx
@@ -0,0 +1,44 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
+
+// COMPONENT IMPORTS
+import { Footer } from 'widgets/Footer';
+import { Navigation } from 'widgets/Navigation';
+import { Main } from 'pages/Main';
+import { Error404 } from 'pages/Error404/Error404';
+import { About } from 'pages/About/';
+import { Contacts } from 'pages/Contacts';
+
+const App = () => {
+    return (
+        <div className='App'>
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>Page title</title>
+                <link rel="canonical" href="http://mysite.com/example" />
+            </Helmet>
+            <Router>
+                <Navigation />
+                <Routes>
+                    <Route
+                        path='/'
+                        element={<Main />}
+                    />
+                    <Route
+                        path='/about'
+                        element={<About />}
+                    />
+                    <Route
+                        path='/contacts'
+                        element={<Contacts />}
+                    />
+                    {/* COMPONENT ROUTES */}
+                    <Route path='*' element={<Error404 />} />
+                </Routes>
+                <Footer />
+            </Router>
+        </div>
+    );
+};
+
+export default App;

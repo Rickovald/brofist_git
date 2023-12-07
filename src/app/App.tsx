d278fde87951eb1c63d0cf1df1e04e56c4fd1f94
Rick <rikonvald@gmail.com>
2023-12-07 15:21:08 +0300

    react query init

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,42 +1,40 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Helmet } from 'react-helmet';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 // COMPONENT IMPORTS
+import { UserRepository } from 'pages/UserRepository/UserRepository';
 import { Footer } from 'widgets/Footer';
 import { Navigation } from 'widgets/Navigation';
-import { Main } from 'pages/Main';
 import { Error404 } from 'pages/Error404/Error404';
-import { About } from 'pages/About/';
-import { Contacts } from 'pages/Contacts';
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Main } from 'pages/Main';
 const App = () => {
+    const queryClient = new QueryClient();
+
     return (
         <div className='App'>
-            <Helmet>
-                <meta charSet="utf-8" />
-                <title>Page title</title>
-                <link rel="canonical" href="http://mysite.com/example" />
-            </Helmet>
-            <Router>
-                <Navigation />
-                <Routes>
-                    <Route
-                        path='/'
-                        element={<Main />}
-                    />
-                    <Route
-                        path='/about'
-                        element={<About />}
-                    />
-                    <Route
-                        path='/contacts'
-                        element={<Contacts />}
-                    />
-                    {/* COMPONENT ROUTES */}
-                    <Route path='*' element={<Error404 />} />
-                </Routes>
-                <Footer />
-            </Router>
+            <HelmetProvider>
+                <Helmet>
+                    <meta charSet="utf-8" />
+                    <title>Page title</title>
+                    <link rel="canonical" href="http://mysite.com/example" />
+                </Helmet>
+                <QueryClientProvider client={queryClient}>
+                    <Router>
+                        <Navigation />
+                        <Routes>
+                            <Route
+                                path='/'
+                                element={<Main />}
+                            />
+                            {/* COMPONENT ROUTES */}
+                            <Route path='/:username' element={<UserRepository />} />
+                            <Route path='*' element={<Error404 />} />
+                        </Routes>
+                        <Footer />
+                    </Router>
+                </QueryClientProvider>
+            </HelmetProvider>
         </div>
     );
 };
